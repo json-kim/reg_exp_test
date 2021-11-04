90c3987b086a055c3e613cb0de237156619d5cd5
json-kim <flutter_dart@kakao.com>
2021-11-04 22:37:20 +0900

    feat(init): 프로젝트 생성

---
+++ bin/reg_exp_test.dart
@@ -0,0 +1,9 @@
+void main() {
+  // raw string(특수문자 무시) 문자열 앞에 r 붙이기
+  String rawStr = r'\n';
+  // 일반 스트링
+  String str = '\n';
+
+  print(rawStr);
+  print(str);
+}
---
+++ test/reg_exp_test_test.dart
@@ -0,0 +1,8 @@
+import 'package:reg_exp_test/reg_exp_test.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
